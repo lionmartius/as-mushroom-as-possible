520b2067babfe83d37b4dfe11520a1e9d445fb96
Lion Rafael Martius <146541125+lionmartius@users.noreply.github.com>
2024-05-30 12:55:03 +0100

    Update 1_chi-sqr.test_merge.r

--- 1_chi-sqr.test_merge.r
+++ 1_chi-sqr.test_merge.r
@@ -1,2 +1,8 @@
-# chi-sqr test
+# What do we need for successful woodland expansion?
+# - As Mushroom as possible
+# Ectomycorrhizal grove formation and implications for woodland expansion 
+# Merged Dataset - EcM Group Formation around Salix 
+# Lion R. Martius
+# Edinburgh, 25/03/2024
+# Chi-sqr test
 
